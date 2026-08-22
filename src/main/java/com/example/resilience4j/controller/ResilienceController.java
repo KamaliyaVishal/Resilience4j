d9ccfab535f67169e9d567c466a2a669b11e0966
Vishal <vishal.kamaliya.er@gmail.com>
2026-08-23 04:09:30 +0530

    Bulkhead

--- src/main/java/com/example/resilience4j/controller/ResilienceController.java
+++ src/main/java/com/example/resilience4j/controller/ResilienceController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CompletableFuture;
+
 @RestController
 public class ResilienceController {
 
@@ -21,6 +23,24 @@ public class ResilienceController {
     @GetMapping("/retry")
     public ResponseEntity<String> doRetryRemoteCall() {
         String result = resilienceService.doRetryRemoteCall();
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/bulkheadSemaphore")
+    public ResponseEntity<String> bulkheadSemaphore() {
+        String result = resilienceService.bulkheadSemaphore();
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/bulkheadThreadPool")
+    public ResponseEntity<String> bulkheadThreadPool() {
+        CompletableFuture<ResponseEntity<String>> result = resilienceService.bulkheadThreadPool();
+        return ResponseEntity.ok(result.toString());
+    }
+
+    @GetMapping("/doBulkheadCalls")
+    public ResponseEntity<String> doBulkheadCalls() {
+        String result = resilienceService.doBulkheadCalls();
         return ResponseEntity.ok(result);
     }
 
--- src/main/java/com/example/resilience4j/service/ResilienceService.java
+++ src/main/java/com/example/resilience4j/service/ResilienceService.java
@@ -1,10 +1,16 @@
 package com.example.resilience4j.service;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class ResilienceService {
@@ -17,6 +23,8 @@ public class ResilienceService {
     private static String remoteServiceURL = "http://localhost:8080/test";
     private static String retryURL = "http://localhost:8090/retry";
     private static String remoteServiceDown = "The remote service is currently unavailable. Please try again after some time.";
+    private static String bulkheadCallsApiUrl = "http://localhost:8090/doBulkheadCalls";
+
 
     @CircuitBreaker(name = "testCircuitBreaker", fallbackMethod = "fallbackForCircuitBreaker")
     public String executeRemoteCall() {
@@ -34,7 +42,7 @@ public class ResilienceService {
         return restTemplate.getForObject(remoteServiceURL, String.class);
     }
 
-    public String fallbackForRetry(Exception e) {
+    public String fallbackForRetry(String id, Throwable e) {
         return remoteServiceDown;
     }
 
@@ -51,4 +59,41 @@ public class ResilienceService {
         lastInvocationTime = currentTime;
     }
 
+    @Bulkhead(name = "testBulkHeadSemaphore", fallbackMethod = "fallbackForBulkheadSemaphore")
+    public String bulkheadSemaphore() {
+        System.out.println("Bulkhead semaphore");
+        ResponseEntity<String> response = restTemplate.getForEntity(bulkheadCallsApiUrl, String.class);
+        System.out.println("Thread: " + Thread.currentThread().getName());
+        return response.getBody();
+    }
+
+    public ResponseEntity<String> fallbackForBulkheadSemaphore(String id, Throwable e) {
+        return new ResponseEntity<>("Semaphore::Too many requests", HttpStatus.TOO_MANY_REQUESTS);
+    }
+
+    public String doBulkheadCalls() {
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return "BulkheadCall Success!";
+    }
+
+    @Bulkhead(name = "testBulkheadThreadPool", type = Bulkhead.Type.THREADPOOL, fallbackMethod = "fallbackForBulkheadThreadPool")
+    public CompletableFuture<ResponseEntity<String>> bulkheadThreadPool() {
+        System.out.println("Bulkhead thread pool");
+        return CompletableFuture.supplyAsync(() -> {
+            ResponseEntity<String> response = restTemplate.getForEntity(bulkheadCallsApiUrl, String.class);
+            System.out.println("Thread: " + Thread.currentThread().getName());
+            System.out.println("response: " + response.getBody());
+            return ResponseEntity.ok(response.getBody());
+        });
+    }
+
+    public CompletableFuture<ResponseEntity<String>> fallbackForBulkheadThreadPool(Throwable throwable) {
+        return CompletableFuture.completedFuture(
+                ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                        .body("bulkheadThreadPool::Too Many Requests."));
+    }
 }
